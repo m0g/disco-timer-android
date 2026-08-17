b81001ceb09f559d39d48bc268abb8382f8bc2de
Loïc Nogues <nogues.loic@gmail.com>
2026-08-17 17:52:50 +0200

    Update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.loicnogues.discotimer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
